153fd4985f75f757828abcd06366a52ea6301e50
chsidev <127224337+chsidev@users.noreply.github.com>
2025-07-10 15:23:46 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: true,
     apiUrl: 'https://api.bawlsonu.life/api',
     communityWallet: 'Cd3XVDGohMUTSm3LC7eXYAtZDsF8FnxbdbBaTSSUQM2',
-    tokenMint: 'GAFQP8vSdDsHcjbBapN794CNEp5Wo6scqSDMSzDHpump',
+    tokenMint: 'DxUTmqRt49dNsKwM1kzrquMpKsnyMGEpGKeZg1YfUdgJ',
+    // communityWallet: 'Cd3XVDGohMUTSm3LC7eXYAtZDsF8FnxbdbBaTSSUQM2',
+    // tokenMint: 'GAFQP8vSdDsHcjbBapN794CNEp5Wo6scqSDMSzDHpump',
   };
   
